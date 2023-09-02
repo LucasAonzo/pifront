e8f55b1e0d899100e6c078e505d2b2e3034130af
LucasAonzo <lucasaonzo@gmail.com>
2023-09-02 14:40:22 -0300

    subiendo front

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -9,11 +9,13 @@ export const GET_GAMES_ORDER_RATING = "GET_GAMES_ORDER_RATING";
 export const GET_GAMES_ORDER_ALPHABETIC = "GET_GAMES_ORDER_ALPHABETIC";
 export const GET_GAMES_FROM_API_OR_DB = "GET_GAMES_FROM_API_OR_DB";
 export const GAME_POST = "GAME_POST";
+const URL = "https://piback-production-6752.up.railway.app/";
 
 export const getAllGames = () => {
   return async (dispatch) => {
     try {
-      const response = await axios.get("http://localhost:3001/videogames");
+      // https://piback-production-6752.up.railway.app/videogames
+      const response = await axios.get(URL + "videogames");
       const data = response.data;
       return dispatch({ type: GET_ALL_GAMES, payload: data });
     } catch (error) {
